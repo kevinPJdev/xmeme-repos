84bdd959730a41c92a387379b2ce146f41a21347
Kevin Jacob <kevjacob.dev@gmail.com>
2021-02-14 22:38:28 +0530

    change in index.js

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ const app= express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res) => {
+  pool.query('SELECT * from meme_dtls',
+    function(err,result) {
+      if(err) {
+        console.log(err);
+        res.status(400).send("No Memes to display");
+      }else {
+      res.status(200).send(result.rows);}
+    })  
+})
+
 //Retreive all the memes
 app.get('/memes', (req,res) => {
     pool.query('SELECT * from meme_dtls',
